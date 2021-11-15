8bdafd5aea88075ef14a8aac79f559bf1099154f
Dudeplayz <Dario@ok.de>
2021-11-15 13:40:06 +0100

    Replace commonjs imports by esm

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import type * as MyModule from "../build/module";
-import {ASUtil, ResultObject} from "@assemblyscript/loader";
+import {ASUtil, ResultObject, instantiateSync} from "@assemblyscript/loader";
+import {readFileSync} from "fs";
 
-const fs = require("fs");
-const loader = require("@assemblyscript/loader");
 export const writeHooks = {}
 
 // @ts-ignore
-export const was: ResultObject & { exports: ASUtil & typeof MyModule } = loader.instantiateSync<typeof MyModule>(fs.readFileSync(__dirname + "/../build/untouched.wasm"), {
+export const was: ResultObject & { exports: ASUtil & typeof MyModule } = instantiateSync<typeof MyModule>(readFileSync(__dirname + "/../build/untouched.wasm"), {
     /* imports */
     index: {
         log: (ptr: number) => console.log('WASM-Log: ' + was.exports.__getString(ptr)),
