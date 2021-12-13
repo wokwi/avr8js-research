e7499658902bbbde2b75931885443354f4d2f557
Dudeplayz <Dario@ok.de>
2021-12-13 14:03:55 +0100

    Failing jest configs...

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,24 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.spec.json',
+      // tsConfig: 'tsconfig.spec.json',
+      // useESM: true,
     },
+    transform:{
+      "\\.[jt]sx?$": "babel-jest"
+    },
+    transformIgnorePatterns: [
+      'node_modules/(?!@assemblyscript)'
+    ],
   },
+  // moduleNameMapper: {
+  //   '^(\\.{1,2}/.*)\\.js$': '$1',
+  // },
 };
---
+++ tests/jest/jest-emulator.ts
@@ -0,0 +1,5 @@
+
+
+function describe2(description : string, fn : () => {}) {
+
+}
