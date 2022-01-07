fc649402efe18e2e3b5348bbd24fd2cfef5cf448
Dudeplayz <Dario@ok.de>
2022-01-07 15:40:17 +0100

    Refactor wasm incompatibilities

--- assembly/__tests__/cpu.spec.ts
+++ assembly/__tests__/cpu.spec.ts
@@ -1,6 +1,6 @@
 import {CPU} from "../avr8js/cpu/cpu";
 
-type ITestEvent = [u32, u32]; // Expected cycles, actual cycles
+type ITestEvent = Map<u32, u32>; // Expected cycles, actual cycles
 
 describe('cpu', () => {
     it('should set initial value of SP to the last byte of internal SRAM', () => {
@@ -11,45 +11,46 @@ describe('cpu', () => {
     describe('events', () => {
         it('should execute queued events after the given number of cycles has passed', () => {
             const cpu = new CPU(new Uint16Array(1024), 0x1000);
-            const events: ITestEvent[] = [];
-            for (const i of [1, 4, 10]) {
-                cpu.addClockEvent(() => events.push([i, cpu.cycles]), i);
+            const events: ITestEvent = new Map();
+
+            const values = [1,4,10]
+            for (let j = 0, len = values.length, i = values[j]; j < len; i = values[++j]) {
+                cpu.addClockEvent(() => events.set(i, cpu.cycles), i);
             }
             for (let i = 0; i < 10; i++) {
                 cpu.cycles++;
                 cpu.tick();
             }
-            expect(events).toBe([
-                [1, 1],
-                [4, 4],
-                [10, 10],
-            ]);
+            expect(events.get(1)).toBe(1)
+            expect(events.get(4)).toBe(4)
+            expect(events.get(10)).toBe(10)
         });
 
         it('should correctly sort the events when added in reverse order', () => {
             const cpu = new CPU(new Uint16Array(1024), 0x1000);
-            const events: ITestEvent[] = [];
-            for (const i of [10, 4, 1]) {
-                cpu.addClockEvent(() => events.push([i, cpu.cycles]), i);
+            const events: ITestEvent = new Map();
+
+            const values = [10,4,1]
+            for (let j = 0, len = values.length, i = values[j]; j < len; i = values[++j]) {
+                cpu.addClockEvent(() => events.set(i, cpu.cycles), i);
             }
             for (let i = 0; i < 10; i++) {
                 cpu.cycles++;
                 cpu.tick();
             }
-            expect(events).toBe([
-                [1, 1],
-                [4, 4],
-                [10, 10],
-            ]);
+            expect(events.get(1)).toBe(1)
+            expect(events.get(4)).toBe(4)
+            expect(events.get(10)).toBe(10)
         });
 
         describe('updateClockEvent', () => {
             it('should update the number of cycles for the given clock event', () => {
                 const cpu = new CPU(new Uint16Array(1024), 0x1000);
-                const events: ITestEvent[] = [];
+                const events: ITestEvent = new Map();
                 const callbacks = [];
-                for (const i of [1, 4, 10]) {
-                    callbacks[i] = cpu.addClockEvent(() => events.push([i, cpu.cycles]), i);
+                const values = [1,4,10]
+                for (let j = 0, len = values.length, i = values[j]; j < len; i = values[++j]) {
+                    callbacks[i] = cpu.addClockEvent(() => events.set(i, cpu.cycles), i);
                 }
                 cpu.updateClockEvent(callbacks[4], 2);
                 cpu.updateClockEvent(callbacks[1], 12);
@@ -57,30 +58,27 @@ describe('cpu', () => {
                     cpu.cycles++;
                     cpu.tick();
                 }
-                expect(events).toBe([
-                    [4, 2],
-                    [10, 10],
-                    [1, 12],
-                ]);
+                expect(events.get(4)).toBe(2)
+                expect(events.get(10)).toBe(10)
+                expect(events.get(1)).toBe(12)
             });
 
             describe('clearClockEvent', () => {
                 it('should remove the given clock event', () => {
                     const cpu = new CPU(new Uint16Array(1024), 0x1000);
-                    const events: ITestEvent[] = [];
+                    const events: ITestEvent = new Map();
                     const callbacks = [];
-                    for (const i of [1, 4, 10]) {
-                        callbacks[i] = cpu.addClockEvent(() => events.push([i, cpu.cycles]), i);
+                    const values = [1,4,10]
+                    for (let j = 0, len = values.length, i = values[j]; j < len; i = values[++j]) {
+                        callbacks[i] = cpu.addClockEvent(() => events.set(i, cpu.cycles), i);
                     }
                     cpu.clearClockEvent(callbacks[4]);
                     for (let i = 0; i < 10; i++) {
                         cpu.cycles++;
                         cpu.tick();
                     }
-                    expect(events).toBe([
-                        [1, 1],
-                        [10, 10],
-                    ]);
+                    expect(events.get(1)).toBe(1)
+                    expect(events.get(10)).toBe(10)
                 });
 
                 it('should return false if the provided clock event is not scheduled', () => {
