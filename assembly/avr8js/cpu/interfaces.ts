2d3af7f4126403d22e3cb4716e26794fee3f50d5
Dudeplayz <Dario@ok.de>
2022-01-10 23:19:40 +0100

    Add AVRInterruptConfigImpl

--- assembly/avr8js/cpu/interfaces.ts
+++ assembly/avr8js/cpu/interfaces.ts
@@ -22,4 +22,54 @@ export interface AVRInterruptConfig {
     inverseFlag: boolean;
 }
 
+export class AVRInterruptConfigImpl implements AVRInterruptConfig {
+    private readonly _address: u8;
+    private readonly _enableRegister: u16;
+    private readonly _enableMask: u8;
+    private readonly _flagRegister: u16;
+    private readonly _flagMask: u8;
+    private readonly _constant: boolean;
+    private readonly _inverseFlag: boolean;
+
+
+    constructor(address: u8, enableRegister: u16, enableMask: u8, flagRegister: u16, flagMask: u8, constant: boolean, inverseFlag: boolean) {
+        this._address = address;
+        this._enableRegister = enableRegister;
+        this._enableMask = enableMask;
+        this._flagRegister = flagRegister;
+        this._flagMask = flagMask;
+        this._constant = constant;
+        this._inverseFlag = inverseFlag;
+    }
+
+
+    get address(): u8 {
+        return this._address;
+    }
+
+    get enableRegister(): u16 {
+        return this._enableRegister;
+    }
+
+    get enableMask(): u8 {
+        return this._enableMask;
+    }
+
+    get flagRegister(): u16 {
+        return this._flagRegister;
+    }
+
+    get flagMask(): u8 {
+        return this._flagMask;
+    }
+
+    get constant(): boolean {
+        return this._constant;
+    }
+
+    get inverseFlag(): boolean {
+        return this._inverseFlag;
+    }
+}
+
 export type AVRClockEventCallback = () => void;
