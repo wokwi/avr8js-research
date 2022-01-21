5e06d9891eb108e9fb29cfafdb5f9f45f2c03550
Dario Götze <dario@ok.de>
2022-01-21 10:52:36 +0100

    Remove test functions and classes

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,14 +1,9 @@
 // The entry file of your WebAssembly module.
-import {Class} from "./tests/class";
-
-export * from './tests/class';
 export * from './glue/cpu-exporter';
 export * from './avr8js/cpu/instruction';
 export {CPU} from './avr8js/cpu/cpu';
 export {AVRInterruptConfigImpl} from './avr8js/cpu/interfaces';
-import {compileInstruction} from "./compiler/instruction-compiler";
 
-// CPU specific
 export declare function log(value: string): void;
 
 export function addLoop(start: i32, end: i32): i32 {
@@ -21,14 +16,4 @@ export function addLoop(start: i32, end: i32): i32 {
 
 export function add(a: i32, b: i32): i32 {
     return a + b;
-}
-
-// tests
-
-export function testCompile(): string {
-    return compileInstruction(0, 0, false)
-}
-
-export function newTestInstance(): Class {
-    return new Class("Hello from WASM Class!");
 }
--- assembly/tests/class.ts
+++
@@ -1,5 +0,0 @@
-export class Class {
-    constructor(public str : string) {
-    }
-
-}
