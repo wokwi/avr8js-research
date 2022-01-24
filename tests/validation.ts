9c83e89260b7bbaefcb0dc4d03088b394d85b24e
Dudeplayz <Dario@ok.de>
2022-01-24 18:47:23 +0100

    Remove validation.assertCompile

--- tests/validation.ts
+++ tests/validation.ts
@@ -4,18 +4,10 @@ import * as assert from "assert"
 export function run() {
     console.log("===", "Run validations...", "===")
     assertAssemblyAdd()
-    assertCompile()
     console.log("===", "Validations successful", "===")
 }
 
 function assertAssemblyAdd() {
     console.log("Validate: assembly add")
     assert.strictEqual(wasm.exports.add(1, 2), 3);
-}
-
-function assertCompile() {
-    console.log("Validate: testCompile")
-    const a: string = wasm.exports.__getString(wasm.exports.testCompile());
-    console.log("Type of " + typeof a)
-    console.log("Value " + a)
 }
