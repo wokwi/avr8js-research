c844fdb9d22fbd525b9cc0e0d28cad6100d5d2a3
Dudeplayz <Dario@ok.de>
2022-01-24 18:29:09 +0100

    Remove replaced cpu-exporter functions

--- assembly/glue/cpu-exporter.ts
+++ assembly/glue/cpu-exporter.ts
@@ -1,7 +1,7 @@
 import {CPU} from "../avr8js/cpu/cpu";
 import {avrInstruction} from "../avr8js/cpu/instruction";
 import {avrInterrupt as _avrInterrupt} from '../avr8js/cpu/interrupt';
-import {callClockEventCallback, callWriteHook} from './cpu-bridge';
+import {callClockEventCallback} from './cpu-bridge';
 import {AVRClockEventCallback} from "../../shared/avr8js/cpu/interfaces";
 
 export namespace avr8js {
@@ -18,66 +18,6 @@ export namespace avr8js {
         for (let i: u32 = 0; i < cycles; i++) {
             avrInstruction(cpu)
         }
-    }
-
-    export function getProgBytes(cpu: CPU): Uint8Array {
-        return cpu.progBytes
-    }
-
-    export function getProgMem(cpu: CPU): Uint16Array {
-        return cpu.progMem
-    }
-
-    export function readData(cpu: CPU, addr: u16): u8 {
-        return cpu.readData(addr);
-    }
-
-    export function writeData(cpu: CPU, addr: u16, value: u8, mask: u8 = 0xff): void {
-        return cpu.writeData(addr, value, mask);
-    }
-
-    export function reset(cpu: CPU): void {
-        cpu.reset()
-    }
-
-    export function getSP(cpu: CPU): u16 {
-        return cpu.SP
-    }
-
-    export function setSP(cpu: CPU, value: u16): void {
-        cpu.SP = value
-    }
-
-    export function getPC(cpu: CPU): u32 {
-        return cpu.pc
-    }
-
-    export function setPC(cpu: CPU, pc: u32): void {
-        cpu.pc = pc
-    }
-
-    export function getCycles(cpu: CPU): u32 {
-        return cpu.cycles as u32
-    }
-
-    export function getSREG(cpu: CPU): u8 {
-        return cpu.SREG
-    }
-
-    export function getData(cpu: CPU): Uint8Array {
-        return cpu.data
-    }
-
-    export function getPC22Bits(cpu: CPU): boolean {
-        return cpu.pc22Bits
-    }
-
-    export function tick(cpu: CPU): void {
-        cpu.tick()
-    }
-
-    export function addWriteHook(cpu: CPU, addr: u32): void {
-        cpu.writeHooks.set(addr, callWriteHook);
     }
 
     export function avrInterrupt(cpu: CPU, addr: u8): void {
