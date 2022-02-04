18269d63ec3f98de93e8476143f1f3a92ac05370
Dudeplayz <Dario@ok.de>
2022-02-04 18:22:53 +0100

    Fix missing WASM import functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import type * as MyModule from "../build/module";
 import {ASUtil, ResultObject, instantiateSync} from "@assemblyscript/loader";
 import {readFileSync} from "fs";
+import {u32} from "../build/module";
 
 const modulePath = __dirname + "/../build/untouched.wasm"
 export const writeHooks = {}
@@ -9,11 +10,14 @@ export const writeHooks = {}
 export const wasm: ResultObject & { exports: ASUtil & typeof MyModule } = instantiateSync<typeof MyModule>(readFileSync(modulePath), {
     /* imports */
     index: {
-        log: (ptr: number) => console.log('WASM-Log: ' + wasm.exports.__getString(ptr)),
-        "cpu-bridge": {
-            callWriteHook(value: number, oldValue: number, addr: number, mask: number): boolean {
-                return writeHooks[addr](value, oldValue, addr, mask)
-            }
+        log: (ptr: number) => console.log('WASM-Log: ' + wasm.exports.__getString(ptr))
+    },
+    "cpu-bridge": {
+        callWriteHook(value: number, oldValue: number, addr: number, mask: number): boolean {
+            return writeHooks[addr](value, oldValue, addr, mask)
+        },
+        callClockEventCallback(callbackId: u32) {
+            //NO-OP
         }
     }
 })
