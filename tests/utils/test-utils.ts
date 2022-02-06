a7ab1b4625a45cb36afeb17fa1484f92e08c0160
Dudeplayz <Dario@ok.de>
2022-02-06 15:46:09 +0100

    Add test-utils.ts from wokwi/avr8js

---
+++ tests/utils/test-utils.ts
@@ -0,0 +1,57 @@
+import { CPU } from '../../src/glue/cpu-wrapper';
+import { assemble } from './assembler';
+
+const BREAK_OPCODE = 0x9598;
+
+export function asmProgram(source: string) {
+  const { bytes, errors, lines, labels } = assemble(source);
+  if (errors.length) {
+    throw new Error('Assembly failed: ' + errors);
+  }
+  return { program: new Uint16Array(bytes.buffer), lines, instructionCount: lines.length, labels };
+}
+
+const defaultOnBreak = () => {
+  throw new Error('BREAK instruction encountered');
+};
+
+export class TestProgramRunner {
+  constructor(
+    private readonly cpu: CPU,
+    private readonly onBreak: (cpu: CPU) => void = defaultOnBreak
+  ) {}
+
+  runInstructions(count: number) {
+    const { cpu, onBreak } = this;
+    for (let i = 0; i < count; i++) {
+      if (cpu.progMem[cpu.pc] === BREAK_OPCODE) {
+        onBreak?.(cpu);
+      }
+      cpu.avrInstruction();
+      cpu.tick();
+    }
+  }
+
+  runUntil(predicate: (cpu: CPU) => boolean, maxIterations = 5000) {
+    const { cpu, onBreak } = this;
+    for (let i = 0; i < maxIterations; i++) {
+      if (cpu.progMem[cpu.pc] === BREAK_OPCODE) {
+        onBreak?.(cpu);
+      }
+      if (predicate(cpu)) {
+        return;
+      }
+      cpu.avrInstruction();
+      cpu.tick();
+    }
+    throw new Error('Test program ran for too long, check your predicate');
+  }
+
+  runToBreak() {
+    this.runUntil((cpu) => cpu.progMem[cpu.pc] === BREAK_OPCODE);
+  }
+
+  runToAddress(byteAddr: number) {
+    this.runUntil((cpu) => cpu.pc * 2 === byteAddr);
+  }
+}
