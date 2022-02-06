16de0bdba10fd0ffddb8eb7ae64083e757c2c747
Dudeplayz <Dario@ok.de>
2022-02-06 18:02:28 +0100

    Add JS side usart.ts with passing usart.spec.ts

---
+++ src/peripherals/usart.spec.ts
@@ -0,0 +1,346 @@
+import { CPU } from '../glue/cpu-wrapper';
+import { AVRUSART, usart0Config } from './usart';
+
+const FREQ_16MHZ = 16e6;
+const FREQ_11_0529MHZ = 11059200;
+
+// CPU registers
+const SREG = 95;
+
+// USART0 Registers
+const UCSR0A = 0xc0;
+const UCSR0B = 0xc1;
+const UCSR0C = 0xc2;
+const UBRR0L = 0xc4;
+const UBRR0H = 0xc5;
+const UDR0 = 0xc6;
+
+// Register bit names
+const U2X0 = 2;
+const TXEN = 8;
+const RXEN = 16;
+const UDRIE = 0x20;
+const TXCIE = 0x40;
+const RXC = 0x80;
+const TXC = 0x40;
+const UDRE = 0x20;
+const USBS = 0x08;
+const UPM0 = 0x10;
+const UPM1 = 0x20;
+
+// Interrupt address
+const PC_INT_UDRE = 0x26;
+const PC_INT_TXC = 0x28;
+const UCSZ0 = 2;
+const UCSZ1 = 4;
+const UCSZ2 = 4;
+
+describe('USART', () => {
+  it('should correctly calculate the baudRate from UBRR', () => {
+    const cpu = new CPU(new Uint16Array(1024));
+    const usart = new AVRUSART(cpu, usart0Config, FREQ_11_0529MHZ);
+    cpu.writeData(UBRR0H, 0);
+    cpu.writeData(UBRR0L, 5);
+    expect(usart.baudRate).toEqual(115200);
+  });
+
+  it('should correctly calculate the baudRate from UBRR in double-speed mode', () => {
+    const cpu = new CPU(new Uint16Array(1024));
+    const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+    cpu.writeData(UBRR0H, 3);
+    cpu.writeData(UBRR0L, 64);
+    cpu.writeData(UCSR0A, U2X0);
+    expect(usart.baudRate).toEqual(2400);
+  });
+
+  it('should call onConfigurationChange when the baudRate changes', () => {
+    const cpu = new CPU(new Uint16Array(1024));
+    const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+    const onConfigurationChange = jest.fn();
+    usart.onConfigurationChange = onConfigurationChange;
+
+    cpu.writeData(UBRR0H, 0);
+    expect(onConfigurationChange).toHaveBeenCalled();
+
+    onConfigurationChange.mockClear();
+    cpu.writeData(UBRR0L, 5);
+    expect(onConfigurationChange).toHaveBeenCalled();
+
+    onConfigurationChange.mockClear();
+    cpu.writeData(UCSR0A, U2X0);
+    expect(onConfigurationChange).toHaveBeenCalled();
+  });
+
+  describe('bitsPerChar', () => {
+    it('should return 5-bits per byte when UCSZ = 0', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0C, 0);
+      expect(usart.bitsPerChar).toEqual(5);
+    });
+
+    it('should return 6-bits per byte when UCSZ = 1', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0C, UCSZ0);
+      expect(usart.bitsPerChar).toEqual(6);
+    });
+
+    it('should return 7-bits per byte when UCSZ = 2', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0C, UCSZ1);
+      expect(usart.bitsPerChar).toEqual(7);
+    });
+
+    it('should return 8-bits per byte when UCSZ = 3', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0C, UCSZ0 | UCSZ1);
+      expect(usart.bitsPerChar).toEqual(8);
+    });
+
+    it('should return 9-bits per byte when UCSZ = 7', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0C, UCSZ0 | UCSZ1);
+      cpu.writeData(UCSR0B, UCSZ2);
+      expect(usart.bitsPerChar).toEqual(9);
+    });
+
+    it('should call onConfigurationChange when bitsPerChar change', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      const onConfigurationChange = jest.fn();
+      usart.onConfigurationChange = onConfigurationChange;
+
+      cpu.writeData(UCSR0C, UCSZ0 | UCSZ1);
+      expect(onConfigurationChange).toHaveBeenCalled();
+
+      onConfigurationChange.mockClear();
+      cpu.writeData(UCSR0B, UCSZ2);
+      expect(onConfigurationChange).toHaveBeenCalled();
+
+      onConfigurationChange.mockClear();
+      cpu.writeData(UCSR0B, UCSZ2);
+      expect(onConfigurationChange).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('stopBits', () => {
+    it('should return 1 when USBS = 0', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      expect(usart.stopBits).toEqual(1);
+    });
+
+    it('should return 2 when USBS = 1', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0C, USBS);
+      expect(usart.stopBits).toEqual(2);
+    });
+  });
+
+  describe('parityEnabled', () => {
+    it('should return false when UPM1 = 0', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      expect(usart.parityEnabled).toEqual(false);
+    });
+
+    it('should return true when UPM1 = 1', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0C, UPM1);
+      expect(usart.parityEnabled).toEqual(true);
+    });
+  });
+
+  describe('parityOdd', () => {
+    it('should return false when UPM0 = 0', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      expect(usart.parityOdd).toEqual(false);
+    });
+
+    it('should return true when UPM0 = 1', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0C, UPM0);
+      expect(usart.parityOdd).toEqual(true);
+    });
+  });
+
+  it('should invoke onByteTransmit when UDR0 is written to', () => {
+    const cpu = new CPU(new Uint16Array(1024));
+    const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+    usart.onByteTransmit = jest.fn();
+    cpu.writeData(UCSR0B, TXEN);
+    cpu.writeData(UDR0, 0x61);
+    expect(usart.onByteTransmit).toHaveBeenCalledWith(0x61);
+  });
+
+  describe('txEnable/rxEnable', () => {
+    it('txEnable should equal true when the transitter is enabled', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      usart.onByteTransmit = jest.fn();
+      expect(usart.txEnable).toEqual(false);
+      cpu.writeData(UCSR0B, TXEN);
+      expect(usart.txEnable).toEqual(true);
+    });
+
+    it('rxEnable should equal true when the transitter is enabled', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      usart.onByteTransmit = jest.fn();
+      expect(usart.rxEnable).toEqual(false);
+      cpu.writeData(UCSR0B, RXEN);
+      expect(usart.rxEnable).toEqual(true);
+    });
+  });
+
+  describe('tick()', () => {
+    it('should trigger data register empty interrupt if UDRE is set', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0B, UDRIE | TXEN);
+      cpu.data[SREG] = 0x80; // SREG: I-------
+      cpu.tick();
+      expect(cpu.pc).toEqual(PC_INT_UDRE);
+      expect(cpu.cycles).toEqual(2);
+      expect(cpu.data[UCSR0A] & UDRE).toEqual(0);
+    });
+
+    it('should trigger data TX Complete interrupt if TXCIE is set', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0B, TXCIE | TXEN);
+      cpu.writeData(UDR0, 0x61);
+      cpu.data[SREG] = 0x80; // SREG: I-------
+      cpu.cycles = 1e6;
+      cpu.tick();
+      expect(cpu.pc).toEqual(PC_INT_TXC);
+      expect(cpu.cycles).toEqual(1e6 + 2);
+      expect(cpu.data[UCSR0A] & TXC).toEqual(0);
+    });
+
+    it('should not trigger data TX Complete interrupt if UDR was not written to', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0B, TXCIE | TXEN);
+      cpu.data[SREG] = 0x80; // SREG: I-------
+      cpu.tick();
+      expect(cpu.pc).toEqual(0);
+      expect(cpu.cycles).toEqual(0);
+    });
+
+    it('should not trigger any interrupt if interrupts are disabled', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0B, UDRIE | TXEN);
+      cpu.writeData(UDR0, 0x61);
+      cpu.data[SREG] = 0; // SREG: 0 (disable interrupts)
+      cpu.cycles = 1e6;
+      cpu.tick();
+      expect(cpu.pc).toEqual(0);
+      expect(cpu.cycles).toEqual(1e6);
+      expect(cpu.data[UCSR0A]).toEqual(TXC | UDRE);
+    });
+  });
+
+  describe('onLineTransmit', () => {
+    it('should call onLineTransmit with the current line buffer after every newline', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      usart.onLineTransmit = jest.fn();
+      cpu.writeData(UCSR0B, TXEN);
+      cpu.writeData(UDR0, 0x48); // 'H'
+      cpu.writeData(UDR0, 0x65); // 'e'
+      cpu.writeData(UDR0, 0x6c); // 'l'
+      cpu.writeData(UDR0, 0x6c); // 'l'
+      cpu.writeData(UDR0, 0x6f); // 'o'
+      cpu.writeData(UDR0, 0xa); // '\n'
+      expect(usart.onLineTransmit).toHaveBeenCalledWith('Hello');
+    });
+
+    it('should not call onLineTransmit if no newline was received', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      usart.onLineTransmit = jest.fn();
+      cpu.writeData(UCSR0B, TXEN);
+      cpu.writeData(UDR0, 0x48); // 'H'
+      cpu.writeData(UDR0, 0x69); // 'i'
+      expect(usart.onLineTransmit).not.toHaveBeenCalled();
+    });
+
+    it('should clear the line buffer after each call to onLineTransmit', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      usart.onLineTransmit = jest.fn();
+      cpu.writeData(UCSR0B, TXEN);
+      cpu.writeData(UDR0, 0x48); // 'H'
+      cpu.writeData(UDR0, 0x69); // 'i'
+      cpu.writeData(UDR0, 0xa); // '\n'
+      cpu.writeData(UDR0, 0x74); // 't'
+      cpu.writeData(UDR0, 0x68); // 'h'
+      cpu.writeData(UDR0, 0x65); // 'e'
+      cpu.writeData(UDR0, 0x72); // 'r'
+      cpu.writeData(UDR0, 0x65); // 'e'
+      cpu.writeData(UDR0, 0xa); // '\n'
+      expect(usart.onLineTransmit).toHaveBeenCalledWith('there');
+    });
+  });
+
+  describe('writeByte', () => {
+    it('should return false if called when RX is busy', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0B, RXEN);
+      cpu.writeData(UBRR0L, 103); // baud: 9600
+      expect(usart.writeByte(10)).toEqual(true);
+      expect(usart.writeByte(10)).toEqual(false);
+      cpu.tick();
+      expect(usart.writeByte(10)).toEqual(false);
+    });
+  });
+
+  describe('Integration tests', () => {
+    it('should set the TXC bit after ~1.04mS when baud rate set to 9600', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      cpu.writeData(UCSR0B, TXEN);
+      cpu.writeData(UBRR0L, 103); // baud: 9600
+      cpu.writeData(UDR0, 0x48); // 'H'
+      cpu.cycles += 16000; // 1ms
+      cpu.tick();
+      expect(cpu.data[UCSR0A] & TXC).toEqual(0);
+      cpu.cycles += 800; // 0.05ms
+      cpu.tick();
+      expect(cpu.data[UCSR0A] & TXC).toEqual(TXC);
+    });
+
+    it('should be ready to recieve the next byte after ~1.04ms when baudrate set to 9600', () => {
+      const cpu = new CPU(new Uint16Array(1024));
+      const usart = new AVRUSART(cpu, usart0Config, FREQ_16MHZ);
+      const rxCompleteCallback = jest.fn();
+      usart.onRxComplete = rxCompleteCallback;
+      cpu.writeData(UCSR0B, RXEN);
+      cpu.writeData(UBRR0L, 103); // baud: 9600
+      expect(usart.writeByte(0x42)).toBe(true);
+      cpu.cycles += 16000; // 1ms
+      cpu.tick();
+      expect(cpu.data[UCSR0A] & RXC).toEqual(0); // byte not received yet
+      expect(usart.rxBusy).toBe(true);
+      expect(rxCompleteCallback).not.toHaveBeenCalled();
+      cpu.cycles += 800; // 0.05ms
+      cpu.tick();
+      expect(cpu.data[UCSR0A] & RXC).toEqual(RXC);
+      expect(usart.rxBusy).toBe(false);
+      expect(rxCompleteCallback).toHaveBeenCalled();
+      expect(cpu.readData(UDR0)).toEqual(0x42);
+      expect(cpu.readData(UDR0)).toEqual(0);
+    });
+  });
+});
---
+++ src/peripherals/usart.ts
@@ -0,0 +1,277 @@
+/**
+ * AVR-8 USART Peripheral
+ * Part of AVR8js
+ * Reference: http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega48A-PA-88A-PA-168A-PA-328-P-DS-DS40002061A.pdf
+ *
+ * Copyright (C) 2019, 2020, 2021 Uri Shaked
+ */
+
+import { CPU } from '../glue/cpu-wrapper';
+import { u8 } from '../../build/module';
+import {AVRInterruptConfig} from "../glue/interfaces";
+
+export interface USARTConfig {
+  rxCompleteInterrupt: u8;
+  dataRegisterEmptyInterrupt: u8;
+  txCompleteInterrupt: u8;
+
+  UCSRA: u8;
+  UCSRB: u8;
+  UCSRC: u8;
+  UBRRL: u8;
+  UBRRH: u8;
+  UDR: u8;
+}
+
+export const usart0Config: USARTConfig = {
+  rxCompleteInterrupt: 0x24,
+  dataRegisterEmptyInterrupt: 0x26,
+  txCompleteInterrupt: 0x28,
+  UCSRA: 0xc0,
+  UCSRB: 0xc1,
+  UCSRC: 0xc2,
+  UBRRL: 0xc4,
+  UBRRH: 0xc5,
+  UDR: 0xc6,
+};
+
+export type USARTTransmitCallback = (value: u8) => void;
+export type USARTLineTransmitCallback = (value: string) => void;
+export type USARTConfigurationChangeCallback = () => void;
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// Register bits:
+const UCSRA_RXC = 0x80; // USART Receive Complete
+const UCSRA_TXC = 0x40; // USART Transmit Complete
+const UCSRA_UDRE = 0x20; // USART Data Register Empty
+const UCSRA_FE = 0x10; // Frame Error
+const UCSRA_DOR = 0x8; // Data OverRun
+const UCSRA_UPE = 0x4; // USART Parity Error
+const UCSRA_U2X = 0x2; // Double the USART Transmission Speed
+const UCSRA_MPCM = 0x1; // Multi-processor Communication Mode
+const UCSRA_CFG_MASK = UCSRA_U2X;
+const UCSRB_RXCIE = 0x80; // RX Complete Interrupt Enable
+const UCSRB_TXCIE = 0x40; // TX Complete Interrupt Enable
+const UCSRB_UDRIE = 0x20; // USART Data Register Empty Interrupt Enable
+const UCSRB_RXEN = 0x10; // Receiver Enable
+const UCSRB_TXEN = 0x8; // Transmitter Enable
+const UCSRB_UCSZ2 = 0x4; // Character Size 2
+const UCSRB_RXB8 = 0x2; // Receive Data Bit 8
+const UCSRB_TXB8 = 0x1; // Transmit Data Bit 8
+const UCSRB_CFG_MASK = UCSRB_UCSZ2 | UCSRB_RXEN | UCSRB_TXEN;
+const UCSRC_UMSEL1 = 0x80; // USART Mode Select 1
+const UCSRC_UMSEL0 = 0x40; // USART Mode Select 0
+const UCSRC_UPM1 = 0x20; // Parity Mode 1
+const UCSRC_UPM0 = 0x10; // Parity Mode 0
+const UCSRC_USBS = 0x8; // Stop Bit Select
+const UCSRC_UCSZ1 = 0x4; // Character Size 1
+const UCSRC_UCSZ0 = 0x2; // Character Size 0
+const UCSRC_UCPOL = 0x1; // Clock Polarity
+/* eslint-enable @typescript-eslint/no-unused-vars */
+
+const rxMasks = {
+  5: 0x1f,
+  6: 0x3f,
+  7: 0x7f,
+  8: 0xff,
+  9: 0xff,
+};
+export class AVRUSART {
+  public onByteTransmit: USARTTransmitCallback | null = null;
+  public onLineTransmit: USARTLineTransmitCallback | null = null;
+  public onRxComplete: (() => void) | null = null;
+  public onConfigurationChange: USARTConfigurationChangeCallback | null = null;
+
+  private rxBusyValue = false;
+  private rxByte = 0;
+  private lineBuffer = '';
+
+  // Interrupts
+  private RXC: AVRInterruptConfig = {
+    address: this.config.rxCompleteInterrupt,
+    flagRegister: this.config.UCSRA,
+    flagMask: UCSRA_RXC,
+    enableRegister: this.config.UCSRB,
+    enableMask: UCSRB_RXCIE,
+    constant: true,
+    inverseFlag: false
+  };
+  private UDRE: AVRInterruptConfig = {
+    address: this.config.dataRegisterEmptyInterrupt,
+    flagRegister: this.config.UCSRA,
+    flagMask: UCSRA_UDRE,
+    enableRegister: this.config.UCSRB,
+    enableMask: UCSRB_UDRIE,
+    constant: false,
+    inverseFlag: false
+  };
+  private TXC: AVRInterruptConfig = {
+    address: this.config.txCompleteInterrupt,
+    flagRegister: this.config.UCSRA,
+    flagMask: UCSRA_TXC,
+    enableRegister: this.config.UCSRB,
+    enableMask: UCSRB_TXCIE,
+    constant: false,
+    inverseFlag: false
+  };
+
+  constructor(private cpu: CPU, private config: USARTConfig, private freqHz: number) {
+    this.reset();
+    this.cpu.writeHooks[config.UCSRA] = (value, oldValue) => {
+      cpu.data[config.UCSRA] = value & (UCSRA_MPCM | UCSRA_U2X);
+      cpu.clearInterruptByFlag(this.TXC, value);
+      if ((value & UCSRA_CFG_MASK) !== (oldValue & UCSRA_CFG_MASK)) {
+        this.onConfigurationChange?.();
+      }
+      return true;
+    };
+    this.cpu.writeHooks[config.UCSRB] = (value, oldValue) => {
+      cpu.updateInterruptEnable(this.RXC, value);
+      cpu.updateInterruptEnable(this.UDRE, value);
+      cpu.updateInterruptEnable(this.TXC, value);
+      if (value & UCSRB_RXEN && oldValue & UCSRB_RXEN) {
+        cpu.clearInterrupt(this.RXC);
+      }
+      if (value & UCSRB_TXEN && !(oldValue & UCSRB_TXEN)) {
+        // Enabling the transmission - mark UDR as empty
+        cpu.setInterruptFlag(this.UDRE);
+      }
+      cpu.data[config.UCSRB] = value;
+      if ((value & UCSRB_CFG_MASK) !== (oldValue & UCSRB_CFG_MASK)) {
+        this.onConfigurationChange?.();
+      }
+      return true;
+    };
+    this.cpu.writeHooks[config.UCSRC] = (value) => {
+      cpu.data[config.UCSRC] = value;
+      this.onConfigurationChange?.();
+      return true;
+    };
+    this.cpu.readHooks[config.UDR] = () => {
+      const mask = rxMasks[this.bitsPerChar] ?? 0xff;
+      const result = this.rxByte & mask;
+      this.rxByte = 0;
+      this.cpu.clearInterrupt(this.RXC);
+      return result;
+    };
+    this.cpu.writeHooks[config.UDR] = (value) => {
+      if (this.onByteTransmit) {
+        this.onByteTransmit(value);
+      }
+      if (this.onLineTransmit) {
+        const ch = String.fromCharCode(value);
+        if (ch === '\n') {
+          this.onLineTransmit(this.lineBuffer);
+          this.lineBuffer = '';
+        } else {
+          this.lineBuffer += ch;
+        }
+      }
+      this.cpu.addClockEvent(() => {
+        cpu.setInterruptFlag(this.UDRE);
+        cpu.setInterruptFlag(this.TXC);
+      }, this.cyclesPerChar);
+      this.cpu.clearInterrupt(this.TXC);
+      this.cpu.clearInterrupt(this.UDRE);
+    };
+    this.cpu.writeHooks[config.UBRRH] = (value) => {
+      this.cpu.data[config.UBRRH] = value;
+      this.onConfigurationChange?.();
+      return true;
+    };
+    this.cpu.writeHooks[config.UBRRL] = (value) => {
+      this.cpu.data[config.UBRRL] = value;
+      this.onConfigurationChange?.();
+      return true;
+    };
+  }
+
+  reset() {
+    this.cpu.data[this.config.UCSRA] = UCSRA_UDRE;
+    this.cpu.data[this.config.UCSRB] = 0;
+    this.cpu.data[this.config.UCSRC] = UCSRC_UCSZ1 | UCSRC_UCSZ0; // default: 8 bits per byte
+    this.rxBusyValue = false;
+    this.rxByte = 0;
+    this.lineBuffer = '';
+  }
+
+  get rxBusy() {
+    return this.rxBusyValue;
+  }
+
+  writeByte(value: number, immediate = false) {
+    const { cpu } = this;
+    if (this.rxBusyValue || !this.rxEnable) {
+      return false;
+    }
+    if (immediate) {
+      this.rxByte = value;
+      cpu.setInterruptFlag(this.RXC);
+      this.onRxComplete?.();
+    } else {
+      this.rxBusyValue = true;
+      cpu.addClockEvent(() => {
+        this.rxBusyValue = false;
+        this.writeByte(value, true);
+      }, this.cyclesPerChar);
+      return true;
+    }
+  }
+
+  private get cyclesPerChar() {
+    const symbolsPerChar = 1 + this.bitsPerChar + this.stopBits + (this.parityEnabled ? 1 : 0);
+    return (this.UBRR + 1) * this.multiplier * symbolsPerChar;
+  }
+
+  private get UBRR() {
+    const { UBRRH, UBRRL } = this.config;
+    return (this.cpu.data[UBRRH] << 8) | this.cpu.data[UBRRL];
+  }
+
+  private get multiplier() {
+    return this.cpu.data[this.config.UCSRA] & UCSRA_U2X ? 8 : 16;
+  }
+
+  get rxEnable() {
+    return !!(this.cpu.data[this.config.UCSRB] & UCSRB_RXEN);
+  }
+
+  get txEnable() {
+    return !!(this.cpu.data[this.config.UCSRB] & UCSRB_TXEN);
+  }
+
+  get baudRate() {
+    return Math.floor(this.freqHz / (this.multiplier * (1 + this.UBRR)));
+  }
+
+  get bitsPerChar() {
+    const ucsz =
+      ((this.cpu.data[this.config.UCSRC] & (UCSRC_UCSZ1 | UCSRC_UCSZ0)) >> 1) |
+      (this.cpu.data[this.config.UCSRB] & UCSRB_UCSZ2);
+    switch (ucsz) {
+      case 0:
+        return 5;
+      case 1:
+        return 6;
+      case 2:
+        return 7;
+      case 3:
+        return 8;
+      default: // 4..6 are reserved
+      case 7:
+        return 9;
+    }
+  }
+
+  get stopBits() {
+    return this.cpu.data[this.config.UCSRC] & UCSRC_USBS ? 2 : 1;
+  }
+
+  get parityEnabled() {
+    return this.cpu.data[this.config.UCSRC] & UCSRC_UPM1 ? true : false;
+  }
+
+  get parityOdd() {
+    return this.cpu.data[this.config.UCSRC] & UCSRC_UPM0 ? true : false;
+  }
+}
